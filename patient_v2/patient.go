df5de1e907b313115a5f486256d2bfbb5380c4c7
belltanellc <belltanellc@gmail.com>
2016-11-21 08:54:10 -0500

    eqrf

--- patient_v2/patient.go
+++ patient_v2/patient.go
@@ -32,6 +32,7 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
+    fmt.Printf("Starting Simple chaincode: %s", "Starting")
 }
 
 // Init resets all the things
